9c4d32bca8d8e985ebd3d13d18f78ebbdf14b5eb
Achraf622-cpu <hanzazachraf581@gmail.com>
2026-03-09 11:04:16 +0000

    feat(STRM-FS-028): implement RouteValidator.java

---
+++ streamvibe-backend/gateway-service/src/main/java/com/streamvibe/gatewayservice/filter/RouteValidator.java
@@ -0,0 +1,23 @@
+package com.streamvibe.gatewayservice.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/auth/token",
+            "/api/auth/login",
+            "/api/auth/validate",
+            "/api/users/register",
+            "/api/videos",
+            "/eureka");
+
+    public Predicate<ServerHttpRequest> isSecured = request -> openApiEndpoints
+            .stream()
+            .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
